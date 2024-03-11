0391544b19320cc4efdf809907da87b35c121042
davestroud <david@davidstroud.me>
2024-03-11 09:39:31 -0400

    after git changes

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ import numpy as np
 
 def init_q_table(states, actions):
     return {state: {action: 0.0 for action in actions} for state in states}
+
+
+# Function to select the next action
+def select_next_action(q_table, state, epsilon):
+    if np.random.random() < epsilon:
+        return np.random.choice(list(q_table[state].keys()))
+    else:
+        return max(q_table[state], key=q_table[state].get)
